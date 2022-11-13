ee7dffe44b5d4e5715996020df222dc485213826
Adnan Niaz <sanixstudio@gmail.com>
2022-11-12 22:20:20 -0800

    chor: minor change

--- src/fetchers/index.js
+++ src/fetchers/index.js
@@ -1,47 +1,48 @@
 import { http } from "./http";
+import axios from 'axios';
 
 const getInformation = async () => {
-  let res = await http.get(`/information.json`);
+  let res = await axios.get(`/information.json`);
   return res.data;
 };
 
 const getServices = async () => {
-  let res = await http.get(`/services.json`, { method: 'GET' });
+  let res = await axios.get(`/services.json`);
   return res.data;
 };
 
 const getTechskills = async () => {
-  let res = await http.get(`/techskills.json`, { method: 'GET' });
+  let res = await axios.get(`/techskills.json`);
   return res.data;
 };
 
 const getLanguageskills = async () => {
-  let res = await http.get(`/languageskills.json`, { method: 'GET' });
+  let res = await axios.get(`/languageskills.json`);
   return res.data;
 };
 
 const getPortfolioFilters = async () => {
-  let res = await http.get(`/portfoliofilters.json`, { method: 'GET' });
+  let res = await axios.get(`/portfoliofilters.json`);
   return res.data;
 };
 
 const getPortfolios = async () => {
-  let res = await http.get(`/portfolios.json`, { method: 'GET' });
+  let res = await axios.get(`/portfolios.json`);
   return res.data;
 };
 
 const getJobExperience = async () => {
-  let res = await http.get(`/jobexperience.json`, { method: 'GET' });
+  let res = await axios.get(`/jobexperience.json`);
   return res.data;
 };
 
 const getEducationBackground = async () => {
-  let res = await http.get(`/educationbackground.json`, { method: 'GET' });
+  let res = await axios.get(`/educationbackground.json`);
   return res.data;
 };
 
 const getClientReviews = async () => {
-  let res = await http.get(`/clientsreview.json`, { method: 'GET' });
+  let res = await axios.get(`/clientsreview.json`);
   return res.data;
 };
 
