ae0204eae70023f11f3843a9a81bed3363d92adc
Adnan Niaz <sanixstudio@gmail.com>
2022-11-12 23:52:34 -0800

    fix: locating issue

--- src/fetchers/http.js
+++ src/fetchers/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || process.env.PORT,
+    baseURL: process.env.PORT || process.env.NEXT_PUBLIC_API_URL,
     headers: {
         Accept: "application/json",
         "Access-Control-Allow-Origin": "*",
