55b5480255e076d949b7abe28c17e3d17471a480
Adnan Niaz <sanixstudio@gmail.com>
2022-11-12 22:37:42 -0800

    fix: still fixing the issue

--- next.config.js
+++ next.config.js
@@ -7,31 +7,48 @@
 //         }, ]
 //     },
 // }
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// const withPlugins = require("next-compose-plugins");
+// const withPWA = require("next-pwa");
+// const runtimeCaching = require("next-pwa/cache");
 
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        deviceSizes: [640, 768, 1024, 1280, 1536],
-        loader: "custom",
-        path: "/",
-    },
-};
+// const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//         deviceSizes: [640, 768, 1024, 1280, 1536],
+//         loader: "custom",
+//         path: "/",
+//     },
+// };
 
-module.exports = withPlugins(
-    [
-        [
-            withPWA,
-            {
-                pwa: {
-                    disable: process.env.NODE_ENV === "development",
-                    dest: "public",
-                    runtimeCaching,
-                },
-            },
-        ],
-    ],
-    nextConfig
-);
+// module.exports = withPlugins(
+//     [
+//         [
+//             withPWA,
+//             {
+//                 pwa: {
+//                     disable: process.env.NODE_ENV === "development",
+//                     dest: "public",
+//                     runtimeCaching,
+//                 },
+//             },
+//         ],
+//     ],
+//     nextConfig
+// );
+
+module.exports = {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  };
